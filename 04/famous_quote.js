4456b53b7b0e031cd72859ef38f1b29d9e680c9f
Muhammad Hamza Aamir <hamzabhatti782259@gmail.com>
2023-08-12 09:20:28 +0500

    files Add

---
+++ 04/famous_quote.js
@@ -0,0 +1,3 @@
+"use strict";
+let famous_person = "Albert Einstein";
+console.log(`${famous_person} once said,“A person who never made a mistake never tried anything new.”`);
---
+++ 04/famous_quote.ts
@@ -0,0 +1,6 @@
+
+
+let famous_person = "Albert Einstein"
+console.log(`${famous_person} once said,“A person who never made a mistake never tried anything new.”`)
+
+
