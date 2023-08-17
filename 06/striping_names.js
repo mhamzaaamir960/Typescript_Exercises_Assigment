dedc669ce6a4f65864dde73ef6957a29e20dbe61
Muhammad Hamza Aamir <hamzabhatti782259@gmail.com>
2023-08-17 19:39:18 +0500

    Added files

---
+++ 06/striping_names.js
@@ -0,0 +1,5 @@
+"use strict";
+let name_1 = "Hamza";
+console.log(`Name with whitespaces => \t \n ${name_1}  \t \n `);
+let strippedName = name_1.trim();
+console.log(`Striped name => ${strippedName}`);
---
+++ 06/striping_names.ts
@@ -0,0 +1,5 @@
+let name_1: string = "Hamza";
+console.log(`Name with whitespaces => \t \n ${name_1}  \t \n `)
+
+let strippedName:string = name_1.trim();
+console.log(`Striped name => ${strippedName}`);
